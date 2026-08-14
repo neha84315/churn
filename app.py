2a65e9f2ba0136eb4840e0c80988d7583c4ebad6
neha84315 <nrehamanbhai@gmail.com>
2026-08-14 15:18:46 +0530

    Refactor Streamlit app for customer churn prediction

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
 with open('onehot_encoder_geography.pkl', 'rb') as file:
-    onehot_encoder_geo = pickle.load(file)
+    onehot_encoder_geography = pickle.load(file)
 
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
 ## streamlit app
-st.title('Customer Churn PRediction')
+st.title('Customer Churn Prediction')
 
 # User input
-geography = st.selectbox('Geography', onehot_encoder_geogra.categories_[0])
+geography = st.selectbox('Geography', onehot_encoder_geography.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
@@ -47,8 +47,8 @@ input_data = pd.DataFrame({
 })
 
 # One-hot encode 'Geography'
-geo_encoded = onehot_encoder_geography.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geography.get_feature_names_out(['Geography']))
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
 # Combine one-hot encoded columns with input data
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
